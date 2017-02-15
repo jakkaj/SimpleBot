f39b55a3b868399299d7fe55193aa3fbbe6dc17e
Tim Hill <thill@microsoft.com>
2017-02-16 00:32:00 +1100

    Added to comments

--- SimpleIgniteBot/SimpleIgniteBot/QnaMaker/QnaMakerResult.cs
+++ SimpleIgniteBot/SimpleIgniteBot/QnaMaker/QnaMakerResult.cs
@@ -11,13 +11,13 @@ namespace SimpleIgniteBot.QnAMaker
     public class QnaMakerResult
     {
         /// <summary>
-        /// The top answer found in the QnA Service.
+        /// The top answer found in the QnA Maker Service.
         /// </summary>
         [JsonProperty(PropertyName = "answer")]
         public string Answer { get; set; }
 
         /// <summary>
-        /// The score in range [0, 100] corresponding to the top answer found in the QnA    Service.
+        /// The score in range [0, 100] corresponding to the top answer found in the QnA Maker Service.
         /// </summary>
         [JsonProperty(PropertyName = "score")]
         public double Score { get; set; }
